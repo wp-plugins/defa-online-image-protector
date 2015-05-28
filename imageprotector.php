be478dc9f0589deebf98a59d9485d02b770c3c96
juthawong <juthawong@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-28 06:43:56 +0000

    Startup Defa Online Image Protector
    git-svn-id: https://plugins.svn.wordpress.org/defa-online-image-protector/trunk@1169118 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ imageprotector.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: Defa Online Image Protector Free Edition
+Plugin URI: http://ww2.juthawong.com/imageprotector
+Description: Protect your image from being downloaded or stolen and hotlink
+Version: 1.0
+Author: Juthawong Naisanguansee
+Author URI: http://www.juthawong.com/
+License: Freeware
+*/
+add_action('wp_head','defago2');
+add_action('wp_footer', 'defaset23');
+
+function defago2()
+{
+include( plugin_dir_path( __FILE__ ) . 'includetop.php');
+
+}
+function defaset23()
+{
+
+include( plugin_dir_path( __FILE__ ) . 'includebottom.php');
+}
+
+
+?>
