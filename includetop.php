b034991d378f0fdd6d009d22678953304c6d29c8
juthawong <juthawong@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-31 10:34:27 +0000

    Make compatible with Defa Protector
    git-svn-id: https://plugins.svn.wordpress.org/defa-online-image-protector/trunk@1171271 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includetop.php
+++ includetop.php
@@ -2,6 +2,10 @@
 //This code has been modified for Image Protector Online
 header('X-UA-Compatible: IE=edge,chrome=1');
 //Written By Juthawong Naisanguansee
+if (ob_get_status())  {
+
+}else{
 ob_start();
+}
 ?>
 <meta http-equiv="imagetoolbar" content="no" />
