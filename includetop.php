69f63002c24866d6cd1e7a1a07c6e639e1a0ad6f
juthawong <juthawong@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-31 10:36:06 +0000

    Make Compatible
    git-svn-id: https://plugins.svn.wordpress.org/defa-online-image-protector/tags/3.2@1171273 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includetop.php
@@ -0,0 +1,11 @@
+<?php
+//This code has been modified for Image Protector Online
+header('X-UA-Compatible: IE=edge,chrome=1');
+//Written By Juthawong Naisanguansee
+if (ob_get_status())  {
+
+}else{
+ob_start();
+}
+?>
+<meta http-equiv="imagetoolbar" content="no" />
